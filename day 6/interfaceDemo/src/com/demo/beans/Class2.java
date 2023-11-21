e02d9b51142d9b469c4ad4908b668a3d1c0c1eae
AtishWalunj <144793371+AtishWalunj@users.noreply.github.com>
2023-11-21 15:10:06 +0530

    Delete day 6/interfaceDemo directory

--- day 6/interfaceDemo/src/com/demo/beans/Class2.java
+++
@@ -1,7 +0,0 @@
-package com.demo.beans;
-
-import com.demo.interfaces.Interface31;
-
-public class Class2 extends Class1 implements Interface31{
-         
-}
--- day 6/interfaceDemo/src/com/demo/interfaces/day 6/interfaceDemo/src/com/demo/interfaces/I1.java
+++
@@ -1,7 +0,0 @@
-package com.demo.interfaces;
-
-public interface I1 {
-	void funct11();
-	int funct12();
-
-}
